44f9f4f405642987df224fd1bc03aa9e63525cc4
Noor ul islam <noorulislam770@gmail.com>
2020-10-10 23:11:30 +0500

    Generalized this time

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
-        int input = 10;
-        int[] list = new int[input];
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the range of sorted number you want : ");
+        int range = in.nextInt();
+        int[] list = new int[range];
 
 
         for (int i = 0; i <  list.length;i++){
@@ -16,8 +19,20 @@ public class Main {
         }
         System.out.print("]\n");
 
-        System.out.println(list);
+
         Search s = new Search(list);
-        s.search(20);
+
+        while (true ){
+            System.out.println("\nEnter the value you want to search : ");
+            int value = in.nextInt();
+            s.search(value);
+
+
+            System.out.println("Do you want to search again '0' -> No anykey-> 'Yes' ");
+            int option = in.nextInt();
+            if (option == 0 ){
+                break;
+            }
+        }
     }
 }
